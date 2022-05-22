627280a5d92813e81b94dbf96dadfc5896fdb516
Csaba Schreiner <schreiner.csaba@gmail.com>
2022-05-22 20:24:02 +0200

    feat(main): add font, organize stuff

--- src/index.js
+++ src/index.js
@@ -4,16 +4,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import {
+	BigCharacter,
+	EgyptianCharacter,
 	Canvas,
 	Content,
 	Etymology,
 	Header,
 	Heading1,
 	Heading2,
+	Heading3,
 	Paragraph,
+	StyledProtoSemiticAleph,
 	Subtitle,
 	Title
-} from './styles.js'
+} from './styles.js';
 
 class Page extends React.Component {
 
@@ -27,19 +31,27 @@ class Page extends React.Component {
         return (
             <Canvas>
 				<Header>
-					<Title>vishvaksara</Title>
+					<Title>ẃixẃákṣáŕá</Title>
+					<Subtitle>vishvaksara</Subtitle>
 					<Etymology>From Sanskrit viśva विश्व (universal) <br/>+ akṣara अक्षर (syllable).</Etymology>
 				</Header>
 				<Content>
 					<Subtitle>An alphabet that makes sense</Subtitle>
-					<Paragraph>Have you ever wondered why letters look the way they do? For example, why does the latin letter "A" look like that? The answer is actually pretty wild.</Paragraph>
-					<Paragraph>A couple milennia ago, in Egyptian hieroglyphs, the word for "Ox" used to be represented by a drawing of a head of an ox, turned sideways: 𓃾.</Paragraph>
-					<Paragraph>In the 19-15th century BC, the Canaanites developed a new script for themselves, basing it on those Egyptian hieroglyphs. Their script described sounds, not concepts. For the sound "A", they chose the hieroglyph for "Ox", because the word for "Ox" sounded like "'alp" in Egyptian.</Paragraph>
-					<Paragraph> Then, hundreds of years later, the Phoenicians developed a new script, basing it on the Canaanite script. They simplified the shape even more. Now, the letter they called "ʾālep" looked like this: 𐤀</Paragraph>
-					<Paragraph> Then, hundreds of years later, the Greek developed a new script, basing it on the Phoenician script. They decided to turn the Phoenician letter on its side. Now, the letter they called "alpha" looked like this: A.</Paragraph>
-					<Paragraph> Then, hundreds of years later, the Etruscans developed a new script, basing it on the Greek script. Their version of this letter looked like: 𐌀.</Paragraph>
-					<Paragraph> Then, hundreds of years later, the Romans developed a new script, basing it on the Etruscan script. Their version of the letter: A.</Paragraph>
-					<Paragraph>When I'm writing this, it's currently the 21st century. There is a human-made robot traveling on the surface of Mars. The robot is called Perseverance. The "A" in its name looks like that because that's how someone drew an ox 5000 years ago.</Paragraph>
+					<Heading3>Who designed the latin alphabet?</Heading3>
+					<Paragraph>Have you ever wondered why letters look the way they do? For example, why does the latin letter "A" look like that? The answer sounded surprising to me.</Paragraph>
+					<Paragraph>Turns out, a couple milennia ago, in Egyptian hieroglyphs, the word for "Ox" used to be represented by a drawing of a head of an ox, turned sideways:</Paragraph>
+					<EgyptianCharacter>𓃾</EgyptianCharacter>
+					<Paragraph>Then, in the 19-15th century BC, the Canaanites developed a new script for themselves, basing it on those Egyptian hieroglyphs. Their script described sounds, not concepts. For the sound "A", they chose the hieroglyph for "Ox", because the word for "Ox" sounded like "'alp" in Egyptian.</Paragraph>
+					<StyledProtoSemiticAleph />
+					<Paragraph> Then, hundreds of years later, the Phoenicians developed a new script, basing it on the Canaanite script. They simplified the shape even more. Now, the letter they called "ʾālep" looked like this:</Paragraph>
+					<BigCharacter>𐤀</BigCharacter>
+					<Paragraph> Then, hundreds of years later, the Greek developed a new script, basing it on the Phoenician script. For some reason, they decided to turn the Phoenician letter on its side. Now, the letter they called "alpha" looked like this: A.</Paragraph>
+					<BigCharacter>A</BigCharacter>
+					<Paragraph> Then, hundreds of years later, the Etruscans developed a new script, basing it on the Greek script. Their version of this letter looked like:</Paragraph>
+					<BigCharacter>𐌀</BigCharacter>
+					<Paragraph> Then, hundreds of years later, the Romans developed a new script, basing it on the Etruscan script. Their version of the letter:</Paragraph>
+					<BigCharacter>A</BigCharacter>
+					<Paragraph>When I'm writing this, it's currently the 21st century. There is a human-made robot traveling on the surface of Mars. The robot is called Perseverance. The "A" in its name looks like that because of... how someone chose to draw an ox 5000 years ago.</Paragraph>
 				</Content>
             </Canvas> 
         );
--- src/styles.js
+++ src/styles.js
@@ -1,5 +1,27 @@
-import styled from 'styled-components';
+import styled from 'styled-components/macro';
 import { darken } from 'polished';
+
+const ProtoSemiticAleph = () => {
+	return (
+	  <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" version="1">
+		<g
+		  fill="none"
+		  fillRule="evenodd"
+		  stroke="#000"
+		  strokeDasharray="none"
+		  strokeLinecap="round"
+		  strokeLinejoin="round"
+		  strokeMiterlimit="4"
+		  strokeOpacity="1"
+		  strokeWidth="3"
+		>
+		  <path d="M11.912 6.833c0 11.551 12.373 12.75 16.252 12.75h18.408c6.518 0 5.986-11.134 5.986-11.134"></path>
+		  <path d="M29.212 20.78l-17.36 32.206L17 58.134l30.41-19.096V20.361"></path>
+		  <path d="M34.286 29.46v-2.54"></path>
+		</g>
+	  </svg>
+	);
+  }
 
 export const colors = {
     red4: '#BB4F32',
@@ -28,6 +50,8 @@ export const Header = styled.div`
 
 export const Content = styled.div`
 	padding: 2rem;
+	max-width: 30rem;
+	margin: 0 auto;
 `;
 
 export const Heading1 = styled.h1`
@@ -42,10 +66,21 @@ export const Heading2 = styled.h2`
 	line-height: 2.5rem;
 `;
 
+export const Heading3 = styled.h3`
+	font-family: 'Montserrat', sans-serif;
+	font-size: 1.5rem;
+	line-height: 1.875rem;
+`;
+
 export const Paragraph = styled.p``;
 
 export const Title = styled(Heading1)`
+	font-family: "Vishvaksara Geometric Bold";
+	font-size: 10rem;
+	line-height: 15rem;
+	font-variant-ligatures: discretionary-ligatures;
 	margin-bottom: 0;
+	margin-top: 3rem;
 `;
 
 export const Subtitle = styled(Heading2)`
@@ -54,6 +89,20 @@ export const Subtitle = styled(Heading2)`
 
 export const Etymology = styled.p`
 	font-size: 1rem;
+`;
+
+export const BigCharacter = styled.div`
+	font-size: 2.5rem;
+`;
+
+export const EgyptianCharacter = styled(BigCharacter)`
+	font-size: 6rem;
+	margin-top: -4rem;
+	margin-bottom: -2rem;
+`;
+
+export const StyledProtoSemiticAleph = styled(ProtoSemiticAleph)`
+	width: 22px;
 `;
 
 export const CharacterCardColumn = styled.div`
