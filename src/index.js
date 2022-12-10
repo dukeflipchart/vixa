94415980358e76c5ee47d8af547a2b5ab3216d71
Csaba Schreiner <duke.flipchart@himmel.hu>
2022-12-10 12:02:48 +0100

    feat(main): start describing english + hungarian

--- src/index.js
+++ src/index.js
@@ -7,11 +7,15 @@ import ConsonantsChart from './images/chart-consonants.svg';
 import VowelsChart from './images/chart-vowels.svg';
 
 import {
-	BigCharacter,
+	BigLetter,
+	BigLetterTable,
+	BigQuoteText,
+	BigQuoteCaption,
 	EgyptianCharacter,
 	Canvas,
 	Content,
 	ContentFullSize,
+	ContentWide,
 	Etymology,
 	Header,
 	Heading1,
@@ -22,11 +26,17 @@ import {
 	Image,
 	ImageWithBorder,
 	InlineV,
+	Neutral4,
 	NumberedList,
 	Paragraph,
 	StyledProtoSemiticAleph,
 	Subtitle,
 	SubtitleV,
+	Table,
+	TableCell,
+	TableCellLetter,
+	TableHeaderCell,
+	TableRow,
 	Title,
 	ToC,
 	ToCItem,
@@ -34,6 +44,25 @@ import {
 	ToCTitle,
 	NumberedListItem
 } from './styles.js';
+
+function TableRowLetter(props) {
+	return (
+		<TableRow>
+			<TableCell><BigLetterTable><InlineV>{props.vishvaksara}</InlineV></BigLetterTable></TableCell>
+			<TableCell><Neutral4>[{props.ipa}]</Neutral4></TableCell>
+			<TableCell>{props.description}</TableCell>
+		</TableRow>
+	);
+}
+
+function BigQuote(props) {
+	return (
+		<>
+			<BigQuoteText>{props.text}</BigQuoteText>
+			<BigQuoteCaption>{props.caption}</BigQuoteCaption>
+		</>
+	);
+}
 
 class Page extends React.Component {
 
@@ -52,8 +81,8 @@ class Page extends React.Component {
 					<Etymology>From Sanskrit viśva विश्व (universal) <br/>+ akṣara अक्षर (syllable).</Etymology>
 				</Header>
 				<Content>
-					<Subtitle>An alphabet that makes sense <InlineV>· ən elfəbet ðet méyks sens</InlineV></Subtitle>
-					<Paragraph>Vishvaksara is a featural constructed alphabet, where every part of a letter describes a certain feature of the sound that letter represents. Because of this system, the more similar two letters are, the more similar their sounds are. This makes it easier to learn than Latin.</Paragraph>
+					<Heading2>An alphabet that makes sense <InlineV>· ən elfəbet ðet méyks sens</InlineV></Heading2>
+					<Paragraph>Vishvaksara is a <em>featural constructed alphabet,</em> where every part of a letter describes a certain feature of the sound that letter represents. Because of this system, the more similar two letters are, the more similar their sounds are. This makes it easier to learn than Latin.</Paragraph>
 					<Paragraph>One letter in Vishvaksara describes exactly one sound. This makes reading and writing straightforward and easy, and makes it so you can pronounce words even if you don't understand them.</Paragraph>
 					<Paragraph>Letterforms in Vishvaksara were chosen so that the most used sounds have the simplest letter shapes, so that writing is as fast as possible. Consonants can be written in a cursive-like way, without lifting the pen, and vowels can be omitted to make writing even faster, something like a shorthand.</Paragraph>
 					<Paragraph>Vishvaksara is designed to be universal: it can be used to write the native languages of at least 3.6 billion people, including English, Mandarin, Yue, Wu, Hindi, Spanish and Standard Arabic.</Paragraph>
@@ -75,12 +104,12 @@ class Page extends React.Component {
 					<Paragraph>Then, in the 19-15th century BC, the Canaanites developed a new script for themselves, basing it on those Egyptian hieroglyphs. Their script described sounds, not concepts. For the sound "A", they chose the hieroglyph for "Ox", because the word for "Ox" sounded like "'alp" in Egyptian.</Paragraph>
 					<StyledProtoSemiticAleph />
 					<Paragraph> Then, hundreds of years later, the Phoenicians developed a new script, basing it on the Canaanite script. They simplified the shape even more. Now, the letter they called "ʾālep" looked like this:</Paragraph>
-					<BigCharacter>𐤀</BigCharacter>
+					<BigLetter>𐤀</BigLetter>
 					<Paragraph> Then, hundreds of years later, the Greek developed a new script, basing it on the Phoenician script. For some reason, they decided to turn the Phoenician letter on its side. Now, the letter they called "alpha" looked like this:</Paragraph>
-					<BigCharacter>A</BigCharacter>
+					<BigLetter>A</BigLetter>
 					<Paragraph>Then the Etruscans copied it, and the Romans copied it, and it became the latin A we all know. People chiseled it, wrote it with pens, adapted it for the printing press and then displayed it on screens.</Paragraph>
 					<Paragraph>When I'm writing this, it's currently the 21st century. There is a human-made robot traveling on the surface of Mars. The robot is called Perseverance. The "A" in its name looks like that because of... how someone chose to draw an ox 5000 years ago.</Paragraph>
-					<Heading3>The benefit of featural alphabets <InlineV>· ðə benefit áf fičərəl elfəbets</InlineV></Heading3>
+					<Heading3>The benefits of featural alphabets <InlineV>· ðə benefits áf fičərəl elfəbets</InlineV></Heading3>
 					<Paragraph>I work in user experience design. My job is to design things so that people can easily understand and use them.</Paragraph>
 					<Paragraph>Perhaps the most important principle in user experience design is <em>consistency</em>. One aspect of consistency is that if two things are similar, they should appear similar.</Paragraph>
 					<Paragraph>Most alphabets don't seem to follow this principle at all, since they weren't designed to. They just evolved naturally, over time. Because of this, similar sounds, like "k" and "g", can look completely different. Sometimes the same letter even denotes an entirely different sound - just compare the "e" letters in the word "different."</Paragraph>
@@ -112,8 +141,375 @@ class Page extends React.Component {
 					<ImageWithBorder src={VowelsChart} alt="Vowels" />
 				</ContentFullSize>
 				<Content>
-					<Paragraph>Vishvaksara writes syllables, not letters. If a consonant is followed by a vowel, they get merged together into one syllable, so the vowel becomes somewhat like a diacritic:</Paragraph>
-					<BigCharacter><InlineV>b + e = be</InlineV></BigCharacter>
+					<Paragraph>Vishvaksara writes syllables, not letters. This means that when a consonant is followed by a vowel, they get merged together into one syllable. The vowel becomes somewhat like a diacritic:</Paragraph>
+					<BigLetter><InlineV>b + e = be</InlineV></BigLetter>
+					<Heading2>Vishvaksara for English <InlineV>· ẃišẃákṣáŕá for ingliš</InlineV></Heading2>
+					<Paragraph>Vishvaksara fully supports the English language, even its rare dental sounds: [θ] (th as in "thief"), and [ð] (th as in "the").</Paragraph>
+				</Content>
+				<ContentWide>
+					<BigQuote
+						text = "oll hyumən biingz ar born fri end ikwəl in digniti end ráyts."
+						caption = 'Example text in General American English: "All human beings are born free and equal in dignity and rights."'
+					/>
+				</ContentWide>
+				<Content>
+					<Paragraph>Since English spelling often diverges from pronounciation, it can take time for English speakers to get used to writing with Vishvaksara. Additionally, each dialect needs to be written differently, since they sound different.</Paragraph>
+					<Heading3>General American</Heading3>
+					<Table>
+						<TableRow>
+							<TableHeaderCell>Letter</TableHeaderCell>
+							<TableHeaderCell>IPA</TableHeaderCell>
+							<TableHeaderCell>GA English sound</TableHeaderCell>
+						</TableRow>
+						<TableRowLetter
+							ipa="æ"
+							description='A as in "apple"'
+							vishvaksara="e"
+						/>
+						<TableRowLetter
+							ipa="ə"
+							description='A as in "comma"'
+							vishvaksara="ə"
+						/>
+						<TableRowLetter
+							ipa="e"
+							description='A as in "change"'
+							vishvaksara="é"
+						/>
+						<TableRowLetter
+							ipa="b"
+							description='B as in "big"'
+							vishvaksara="b"
+						/>
+						<TableRowLetter
+							ipa="k"
+							description='C as in "cat"'
+							vishvaksara="k"
+						/>
+						<TableRowLetter
+							ipa="tʃ"
+							description='CH as in "cheap"'
+							vishvaksara="č"
+						/>
+						<TableRowLetter
+							ipa="x"
+							description='CH as in "loch"'
+							vishvaksara="x"
+						/>
+						<TableRowLetter
+							ipa="d"
+							description='D as in "dog"'
+							vishvaksara="d"
+						/>
+						<TableRowLetter
+							ipa="ɛ"
+							description='E as in "echo"'
+							vishvaksara="e"
+						/>
+						<TableRowLetter
+							ipa="i"
+							description='EE as in "bee"'
+							vishvaksara="i"
+						/>
+						<TableRowLetter
+							ipa="f"
+							description='F as in "four"'
+							vishvaksara="f"
+						/>
+						<TableRowLetter
+							ipa="g"
+							description='G as in "good"'
+							vishvaksara="g"
+						/>
+						<TableRowLetter
+							ipa="h"
+							description='H as in "home"'
+							vishvaksara="h"
+						/>
+						<TableRowLetter
+							ipa="ɪ"
+							description='I as in "kit"'
+							vishvaksara="i"
+						/>
+						<TableRowLetter
+							ipa="dʒ"
+							description='J as in "job"'
+							vishvaksara="j"
+						/>
+						<TableRowLetter
+							ipa="k"
+							description='K as in "kid"'
+							vishvaksara="k"
+						/>
+						<TableRowLetter
+							ipa="l"
+							description='L as in "love"'
+							vishvaksara="l"
+						/>
+						<TableRowLetter
+							ipa="m"
+							description='M as in "mother"'
+							vishvaksara="m"
+						/>
+						<TableRowLetter
+							ipa="n"
+							description='N as in "nice"'
+							vishvaksara="n"
+						/>
+						<TableRowLetter
+							ipa="o"
+							description='O as in "goat"'
+							vishvaksara="o"
+						/>
+						<TableRowLetter
+							ipa="a"
+							description='O as in "comma"'
+							vishvaksara="á"
+						/>
+						<TableRowLetter
+							ipa="u"
+							description='OO as in "foot"'
+							vishvaksara="u"
+						/>
+						<TableRowLetter
+							ipa="p"
+							description='P as in "pet"'
+							vishvaksara="p"
+						/>
+						<TableRowLetter
+							ipa="r"
+							description='R as in "ruckus"'
+							vishvaksara="r"
+						/>
+						<TableRowLetter
+							ipa="s"
+							description='S as in "soup"'
+							vishvaksara="s"
+						/>
+						<TableRowLetter
+							ipa="t"
+							description='T as in "tell"'
+							vishvaksara="t"
+						/>
+						<TableRowLetter
+							ipa="θ"
+							description='TH as in "thing"'
+							vishvaksara="θ"
+						/>
+						<TableRowLetter
+							ipa="ð"
+							description='TH as in "them"'
+							vishvaksara="ð"
+						/>
+						<TableRowLetter
+							ipa="v"
+							description='V as in "vine"'
+							vishvaksara="v"
+						/>
+						<TableRowLetter
+							ipa="w"
+							description='W as in "win"'
+							vishvaksara="w"
+						/>
+						<TableRowLetter
+							ipa="x"
+							description='X as in "explain"'
+							vishvaksara="w"
+						/>
+						<TableRowLetter
+							ipa="j"
+							description='Y as in "you"'
+							vishvaksara="y"
+						/>
+						<TableRowLetter
+							ipa="z"
+							description='Z as in "zoom"'
+							vishvaksara="z"
+						/>
+					</Table><Heading2>Vishvaksara for Hungarian <InlineV>· ẃišẃákṣáŕá for hángeriən</InlineV></Heading2>
+					<Paragraph>Vishvaksara works excellently for Hungarian. Several digraphs and trigraphs, such as "ty," "gy," "ny," or "dzs" are single letters in Vishvaksara, making writing faster and simpler. It also gets rid of the difference between "j" and "ly," a pain point even for several native Hungarian speakers.</Paragraph>
+				</Content>
+				<ContentWide>
+					<BigQuote
+						text = "Minden embeŕi léñ sabadnak sületik, és eďenlö méltošága éš yoga van."
+						caption = 'Example text in Hungarian: "Minden emberi lény szabadnak születik, és egyenlő méltósága és joga van."'
+					/>
+				</ContentWide>
+				<Content>
+					<Paragraph>Vishvaksara differentiates between the "h" in "méh" and the "h" in "hajnal."</Paragraph>
+					<Heading3>General American</Heading3>
+					<Table>
+						<TableRow>
+							<TableHeaderCell>Letter</TableHeaderCell>
+							<TableHeaderCell>IPA</TableHeaderCell>
+							<TableHeaderCell>GA English sound</TableHeaderCell>
+						</TableRow>
+						<TableRowLetter
+							ipa="æ"
+							description='A as in "apple"'
+							vishvaksara="e"
+						/>
+						<TableRowLetter
+							ipa="ə"
+							description='A as in "comma"'
+							vishvaksara="ə"
+						/>
+						<TableRowLetter
+							ipa="e"
+							description='A as in "change"'
+							vishvaksara="é"
+						/>
+						<TableRowLetter
+							ipa="b"
+							description='B as in "big"'
+							vishvaksara="b"
+						/>
+						<TableRowLetter
+							ipa="k"
+							description='C as in "cat"'
+							vishvaksara="k"
+						/>
+						<TableRowLetter
+							ipa="tʃ"
+							description='CH as in "cheap"'
+							vishvaksara="č"
+						/>
+						<TableRowLetter
+							ipa="x"
+							description='CH as in "loch"'
+							vishvaksara="x"
+						/>
+						<TableRowLetter
+							ipa="d"
+							description='D as in "dog"'
+							vishvaksara="d"
+						/>
+						<TableRowLetter
+							ipa="ɛ"
+							description='E as in "echo"'
+							vishvaksara="e"
+						/>
+						<TableRowLetter
+							ipa="i"
+							description='EE as in "bee"'
+							vishvaksara="i"
+						/>
+						<TableRowLetter
+							ipa="f"
+							description='F as in "four"'
+							vishvaksara="f"
+						/>
+						<TableRowLetter
+							ipa="g"
+							description='G as in "good"'
+							vishvaksara="g"
+						/>
+						<TableRowLetter
+							ipa="h"
+							description='H as in "home"'
+							vishvaksara="h"
+						/>
+						<TableRowLetter
+							ipa="ɪ"
+							description='I as in "kit"'
+							vishvaksara="i"
+						/>
+						<TableRowLetter
+							ipa="dʒ"
+							description='J as in "job"'
+							vishvaksara="j"
+						/>
+						<TableRowLetter
+							ipa="k"
+							description='K as in "kid"'
+							vishvaksara="k"
+						/>
+						<TableRowLetter
+							ipa="l"
+							description='L as in "love"'
+							vishvaksara="l"
+						/>
+						<TableRowLetter
+							ipa="m"
+							description='M as in "mother"'
+							vishvaksara="m"
+						/>
+						<TableRowLetter
+							ipa="n"
+							description='N as in "nice"'
+							vishvaksara="n"
+						/>
+						<TableRowLetter
+							ipa="o"
+							description='O as in "goat"'
+							vishvaksara="o"
+						/>
+						<TableRowLetter
+							ipa="a"
+							description='O as in "comma"'
+							vishvaksara="á"
+						/>
+						<TableRowLetter
+							ipa="u"
+							description='OO as in "foot"'
+							vishvaksara="u"
+						/>
+						<TableRowLetter
+							ipa="p"
+							description='P as in "pet"'
+							vishvaksara="p"
+						/>
+						<TableRowLetter
+							ipa="r"
+							description='R as in "ruckus"'
+							vishvaksara="r"
+						/>
+						<TableRowLetter
+							ipa="s"
+							description='S as in "soup"'
+							vishvaksara="s"
+						/>
+						<TableRowLetter
+							ipa="t"
+							description='T as in "tell"'
+							vishvaksara="t"
+						/>
+						<TableRowLetter
+							ipa="θ"
+							description='TH as in "thing"'
+							vishvaksara="θ"
+						/>
+						<TableRowLetter
+							ipa="ð"
+							description='TH as in "them"'
+							vishvaksara="ð"
+						/>
+						<TableRowLetter
+							ipa="v"
+							description='V as in "vine"'
+							vishvaksara="v"
+						/>
+						<TableRowLetter
+							ipa="w"
+							description='W as in "win"'
+							vishvaksara="w"
+						/>
+						<TableRowLetter
+							ipa="x"
+							description='X as in "explain"'
+							vishvaksara="w"
+						/>
+						<TableRowLetter
+							ipa="j"
+							description='Y as in "you"'
+							vishvaksara="y"
+						/>
+						<TableRowLetter
+							ipa="z"
+							description='Z as in "zoom"'
+							vishvaksara="z"
+						/>
+					</Table>
 				</Content>
             </Canvas> 
         );
--- src/styles.js
+++ src/styles.js
@@ -35,6 +35,10 @@ export const colors = {
 	neutral1: `#1C1C1C`
 }
 
+export const Neutral4 = styled.span`
+	color: ${colors.neutral4};
+`;
+
 export const Canvas = styled.div`
     box-sizing: border-box;
     width: 100%;
@@ -50,13 +54,19 @@ export const Header = styled.div`
 `;
 
 export const Content = styled.div`
-	padding: 2rem 2rem 0;
+	padding: 0 2rem;
 	max-width: 33rem;
 	margin: 0 auto;
 `;
 
 export const ContentFullSize = styled.div`
+	padding: 2rem 2rem 0;
+	margin: 0 auto;
+`;
+
+export const ContentWide = styled.div`
 	width: 90%;
+	max-width: 50rem;
 	margin: 0 auto;
 `;
 
@@ -105,7 +115,7 @@ export const Title = styled(Heading1)`
 `;
 
 export const Subtitle = styled(Heading2)`
-	margin-top: -0.5rem;
+	margin-top: 0;
 `;
 
 export const InlineV = styled.span`
@@ -128,6 +138,43 @@ export const NumberedList = styled.ol``;
 
 export const NumberedListItem = styled.li``;
 
+export const Table = styled.table`
+	width: 100%;
+	margin-top: 2rem;
+`;
+
+export const TableHeaderCell = styled.th`
+	border-bottom: 1px solid ${colors.neutral7};
+	color: ${colors.neutral4};
+	font-family: "Montserrat", sans-serif;
+	font-size: 0.75rem;
+	text-align: left;
+	text-transform: uppercase;
+	padding: 0.5rem;
+
+	&:first-of-type {
+		padding-left: 0;
+	}
+
+	&:last-of-type {
+		padding-right: 0;
+	}
+`;
+
+export const TableRow = styled.tr``;
+
+export const TableCell = styled.td`
+	padding: 0.5rem;
+
+	&:first-of-type {
+		padding-left: 0;
+	}
+
+	&:last-of-type {
+		padding-right: 0;
+	}
+`;
+
 export const ToC = styled.div``;
 
 export const ToCList = styled.div``;
@@ -142,11 +189,37 @@ export const Etymology = styled.p`
 	font-size: 1rem;
 `;
 
-export const BigCharacter = styled.div`
+export const BigLetter = styled.span`
+	font-family: "Montserrat", sans-serif;
 	font-size: 2.5rem;
+	line-height: 3.75rem;
 `;
 
-export const EgyptianCharacter = styled(BigCharacter)`
+export const BigLetterTable = styled.span`
+	font-family: "Montserrat", sans-serif;
+	font-size: 1.5rem;
+	line-height: 2.25rem;
+`;
+
+export const BigQuoteText = styled.div`
+background: ${colors.blue4};
+color: ${colors.neutral9};
+font-family: "Vishvaksara Geometric Bold", "Montserrat", sans-serif;
+font-variant-ligatures: discretionary-ligatures;
+font-size: 2.5rem;
+line-height: 3.75rem;
+margin-top: 2rem;
+padding: 1rem 2rem;
+`;
+
+export const BigQuoteCaption = styled.div`
+color: ${colors.neutral4};
+font-size: 0.75rem;
+margin-top: 0.5rem;
+margin-bottom: 2rem;
+`;
+
+export const EgyptianCharacter = styled(BigLetter)`
 	font-size: 6rem;
 	margin-top: -4rem;
 	margin-bottom: -2rem;
